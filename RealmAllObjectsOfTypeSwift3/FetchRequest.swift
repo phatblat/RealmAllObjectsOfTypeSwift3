7ac2e51737b3d6652c7f5e97cf764e88b0b35c21
Ben Chatelain <ben@octop.ad>
2016-07-01 16:20:04 -0600

    Add minimal FetchRequest.swift

---
+++ RealmAllObjectsOfTypeSwift3/FetchRequest.swift
@@ -0,0 +1,27 @@
+// Extracted from SwiftFetchedResultsController pod, part of:
+// https://github.com/Roobiq/RBQFetchedResultsController
+
+import Realm
+import RealmSwift
+
+
+public class FetchRequest<T: Object> {
+    /**
+     Retrieve all the Objects for this fetch request in its realm.
+
+     @return Results for all the objects in the fetch request (not thread-safe).
+     */
+    public func fetchObjects() -> Results<T> {
+
+        let fetchResults = self.realm.objects(T)
+
+        return fetchResults
+    }
+}
+
+// MARK: - Stubs
+extension FetchRequest {
+    var realm: Realm {
+        return try! Realm()
+    }
+}
